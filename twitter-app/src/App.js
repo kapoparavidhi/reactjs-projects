80b07e3891985f0f1ac7d4cb9280a225136248ef
vidhi <vidhi.kapopara@exhibytesolution.com>
2024-01-30 18:23:13 +0530

    demo of graphql using todo list

--- twitter-app/src/App.js
+++ twitter-app/src/App.js
@@ -1,9 +1,11 @@
 import MyComponent from "./components/Userinfo";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <h1 className="text-center text-2xl font-serif">Twitter-App</h1>
+      <Header />
       <MyComponent />
     </>
   );
